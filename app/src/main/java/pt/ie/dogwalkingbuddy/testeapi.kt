5bf44ee3d3695dc22bdc41f19610fca5125888e7
alexsilva9913 <alexsilva9913@gmail.com>
2021-06-07 14:16:05 +0100

    - Notifications Working;

--- app/src/main/java/pt/ie/dogwalkingbuddy/testeapi.kt
+++ app/src/main/java/pt/ie/dogwalkingbuddy/testeapi.kt
@@ -1,11 +1,19 @@
 package pt.ie.dogwalkingbuddy
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.text.Html
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import pt.ie.dogwalkingbuddy.api.WeatherResponse
 import pt.ie.dogwalkingbuddy.api.WeatherService
 import retrofit2.Call
@@ -21,6 +29,13 @@ class testeapi : AppCompatActivity() {
 
     private var weatherData: TextView? = null
 
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var notificationManager: NotificationManager
+    lateinit var builder: Notification.Builder
+    private val channelId = "12345"
+    private val description = "Test Notification"
+    private val notificationId = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.testeapi)
@@ -28,6 +43,7 @@ class testeapi : AppCompatActivity() {
         weatherData = findViewById(R.id.textView)
 
         findViewById<View>(R.id.button).setOnClickListener { getCurrentData() }
+
     }
 
 
@@ -64,6 +80,32 @@ class testeapi : AppCompatActivity() {
                             weatherResponse.weatherList?.get(0)?.mainstate )
 
                     weatherData!!.text = stringBuilder
+
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        val name = "Teste"
+                        val descriptionTeste = "Teste desc"
+                        val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+                        val channel = NotificationChannel(channelId,name,importance).apply {
+                            description = descriptionTeste
+                        }
+
+                        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                        notificationManager.createNotificationChannel(channel)
+                    }
+
+                    val builder = NotificationCompat.Builder(this@testeapi, channelId)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("TEMPO AGORA")
+                        .setContentText(" " + weatherResponse.weatherList?.get(0)?.mainstate)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+                    with(NotificationManagerCompat.from(this@testeapi)) {
+
+                        if(weatherResponse.weatherList?.get(0)?.mainstate == "Rain"){
+                            notify(notificationId, builder.build())
+                        }
+                    }
                 }
             }
 
@@ -71,6 +113,7 @@ class testeapi : AppCompatActivity() {
                 weatherData!!.text = t.message
             }
         })
+
 
     }
 
