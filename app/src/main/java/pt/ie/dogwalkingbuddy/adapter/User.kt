c660b687b2a627092d1420cb26ca20232d49f1d5
Pedro Costa <pedrorebelocosta@gmail.com>
2021-06-18 01:20:50 +0100

    feat(leaderboard) - done

--- app/src/main/java/pt/ie/dogwalkingbuddy/adapter/User.kt
+++
@@ -1,3 +0,0 @@
-package pt.ie.dogwalkingbuddy.adapter
-
-data class User(val pontos: Int = 0)
--- app/src/main/java/pt/ie/dogwalkingbuddy/leaderboard.kt
+++ app/src/main/java/pt/ie/dogwalkingbuddy/leaderboard.kt
@@ -3,10 +3,13 @@ package pt.ie.dogwalkingbuddy
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
-import com.google.firebase.firestore.auth.User
+import kotlinx.android.synthetic.main.content_scrolling.*
+import pt.ie.dogwalkingbuddy.adapter.Player
+import pt.ie.dogwalkingbuddy.adapter.PlayerAdapter
+import pt.ie.dogwalkingbuddy.adapter.loadImg
 
 
 class leaderboard : AppCompatActivity() {
@@ -14,8 +17,8 @@ class leaderboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leaderboard)
-        loadUsers()
-
+        setupRecyclerView()
+        loadPlayers()
     }
 
     private fun setupRecyclerView() {
@@ -26,27 +29,39 @@ class leaderboard : AppCompatActivity() {
         }
     }
 
-    private fun loadUsers() {
+    private fun loadPlayers() {
         val db = FirebaseFirestore.getInstance()
-        db.collection("users")
+        db.collection("user")
             .orderBy("pontos", Query.Direction.DESCENDING)
             .addSnapshotListener({ snapshots, error ->
                 if (error != null) {
-                    Log.d("TAG", error.message)
+                    Log.d("TAG", error.message!!)
                     return@addSnapshotListener
                 }
 
-                val users = snapshots.map{
-                    it.toObject(User::class.java)
+                val players = snapshots?.map{
+                    it.toObject(Player::class.java)
                 }
-                //val champions = users.take(3)
-                showUsersPosition(users)
+                val champions = players?.take(3)
+                if (players != null) {
+                    showPlayersPosition(players)
+                    if (champions != null) {
+                        showChampions(champions)
+                    }
+                }
             })
     }
 
-    private fun showUsersPosition(players: List<User>) {
-        val adapter = recycler.adapter as PlayerAdapter
+    private fun showPlayersPosition(players: List<Player>) {
+        val adapter = recycler_view.adapter as PlayerAdapter
         adapter.addPlayers(players)
+    }
+
+
+    private fun showChampions(championPlayers: List<Player>) {
+        iv_champion1.loadImg(championPlayers[0].photo)
+        iv_champion2.loadImg(championPlayers[1].photo)
+        iv_champion3.loadImg(championPlayers[2].photo)
     }
 
 }
